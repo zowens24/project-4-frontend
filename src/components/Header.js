87c637acaaba877c591780db2217d29d4853bb2a
Luis Rosario <luisalmonter@gmail.com>
2021-01-07 14:59:59 -0500

    adds useState data

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const Header = (props) => {
     return (
-        <h1> Tune It Up</h1>
+        <div className="Header">
+            <h1> Tune It Up</h1>
+            <nav>
+                <ul className="NavLinks">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/songs">Song</Link></li>
+
+                </ul>
+            </nav>
+            
+
+
+        </div>
     )
 }
 export default Header;
--- src/pages/SongPage.js
+++ src/pages/SongPage.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+
+
 
 const SongPage = (props) => {
+
+        const [songsState, setSongsState] = useState({
+            songs: []
+        });
+
+        useEffect(() => {
+            getSongs();
+        }, []);
+
+        function getSongs() {
+            fetch('/songs')
+            .then(res => res.json())
+            .then(data => setSongsState({songs: data}))
+            .catch(error => console.error(error));
+        }
+
+
     return (
-        <h1>Songs</h1>
+        <h1>Hello
+        </h1>
     )
 }
 
